320b78e5f17168f28f85f536396a47bffd9c24ea
erikheuna <charlesricheuna@gmail.com>
2025-09-16 10:08:13 +0100

    Products Router setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import productsRoutes from "./routes/products/index";
 
 const PORT = 3000;
 
@@ -7,6 +8,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// products endpoints
+
+app.use("/products", productsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Ecommerce app listening on http://localhost:${PORT}`);
